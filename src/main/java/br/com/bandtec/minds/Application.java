012f1b4f6044a8eba5b5a6b785aa9784abdcf71e
tigelah <lokoportitmao2009@hotmail.com>
2020-03-01 12:47:02 -0300

    comentando as config do banco antigo

--- src/main/java/br/com/bandtec/minds/Application.java
+++ src/main/java/br/com/bandtec/minds/Application.java
@@ -1,8 +1,14 @@
 package br.com.bandtec.minds;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
 public class Application {
 
